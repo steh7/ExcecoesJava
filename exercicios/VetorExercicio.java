54951524e2ec04eb3d3e932e8a9e07363a01060a
STEH7 <stephanychryscv@gmail.com>
2024-10-28 11:05:43 -0300

    Update VetorExercicio.java

--- exercicios/VetorExercicio.java
+++ exercicios/VetorExercicio.java
@@ -9,7 +9,6 @@ public class VetorExercicio {
         int maior = Integer.MIN_VALUE;
         int menor = Integer.MAX_VALUE;
 
-        // Entrada de dados
         System.out.println("Digite 10 números inteiros:");
         for (int i = 0; i < numeros.length; i++) {
             numeros[i] = scanner.nextInt();
@@ -18,12 +17,10 @@ public class VetorExercicio {
             if (numeros[i] < menor) menor = numeros[i];
         }
 
-        // Exibir soma, maior e menor valores
         System.out.println("Soma dos valores: " + soma);
         System.out.println("Maior valor: " + maior);
         System.out.println("Menor valor: " + menor);
 
-        // Exibir valores e destacar maior e menor
         System.out.println("Valores inseridos:");
         for (int num : numeros) {
             System.out.print(num);
